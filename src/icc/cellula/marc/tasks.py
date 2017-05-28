1f4d3dd782144db57694b2dbf83e2885c8370516
Evgeny Cherkashin <eugeneai@irnok.net>
2017-05-28 14:32:31 +0800

    Added marc isbn recognizer.

--- src/icc/cellula/marc/tasks.py
+++ src/icc/cellula/marc/tasks.py
@@ -3,9 +3,13 @@ from zope.interface import implementer
 from icc.cellula.interfaces import ISingletonTask
 from icc.cellula.workers import Task
 from zope.component import getUtility
-from icc.cellula.interfaces import IRTMetadataIndex
+from icc.cellula.interfaces import IRTMetadataIndex, IWorker
 from icc.contentstorage.interfaces import IContentStorage
+import pymarc
+from marcds.importer.issuerecog import DJVUtoMARC
 import logging
+import tempfile
+
 logger = logging.getLogger('icc.cellula')
 
 _ = _N = MessageFactory("isu.webapp")
@@ -15,7 +19,6 @@ _ = _N = MessageFactory("isu.webapp")
 class IssueDataTask(Task):
 
     def run(self):
-        from marcds.importer.issuerecog import DJVUtoMARC
         logger.info("Ran {}".format(self.__class__))
         metadata = getUtility(IRTMetadataIndex, "elastic")
         count, docs = metadata.query(variant="noisbn", count=10)
@@ -24,15 +27,23 @@ class IssueDataTask(Task):
             logger.debug("No document for processing.")
             return
         storage = getUtility(IContentStorage, name="content")
+        queue_thread = getUtility(IWorker, name="queue")
         for doc in docs:
             logger.debug(doc["File-Name"])
             logger.debug(doc["mimetype"])
-            content_stream = storage.get(doc["id"], stream=True)
-            logger.debug("Content length: {}".format(len(content)))
-            r = DataPageRecognizer(content_stream)
+            content = storage.get(doc["id"])
+            if isinstance(content, (str, bytes)):
+                logger.debug("Content length: {}".format(len(content)))
+                tmp = tempfile.NamedTemporaryFile(dir=queue_thread.tmpdir)
+                tmp.write(content)
+                tmp.flush()
+            else:
+                raise NotImplementedError("type is not supported")
+            r = DJVUtoMARC(tmp.name)
             data = r.issue_data(noexc=True)
             if data is not None:
-                logger.debug("Got some data {}".format(data))
+                logger.debug("Got some data ISBN {}".format(r.isbn))
+                logger.debug("Lines: {}".format(r.lines))
             else:
                 logger.debug("No data found.")
                 continue
